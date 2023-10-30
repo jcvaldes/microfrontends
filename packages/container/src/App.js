a7b33a660775a6a1762bb76f46f8ba16efa42913
Juan Carlos Valdés <idevkingos@gmail.com>
2023-10-30 15:13:15 -0300

    added dashboard subapp

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,19 @@
-import React, { lazy, Suspense, useState } from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React, { lazy, Suspense, useEffect, useState } from 'react'
+import { Router, Route, Switch, Redirect } from 'react-router-dom'
 // import MarketingApp from './components/MarketingApp'
 // import AuthApp from './components/AuthApp'
 import Header from './components/Header'
 import Progress from './components/Progress'
 const MarketingLazy = lazy(() => import('./components/MarketingApp'))
 const AuthLazy = lazy(() => import('./components/AuthApp'))
+const DashboardLazy = lazy(() => import('./components/DashboardApp'))
 
 import {
   StylesProvider,
   createGenerateClassName,
 } from '@material-ui/core/styles'
+
+import { createBrowserHistory } from 'history'
 
 // esto es para que no choquen los estilos en produccion
 // cuando se renombran las clases
@@ -18,10 +21,18 @@ const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 })
 
+const history = createBrowserHistory()
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false)
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push('/dashboard')
+    }
+  }, [isSignedIn])
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header
@@ -37,6 +48,10 @@ export default () => {
               <Route path="/auth">
                 <AuthLazy onSignIn={() => setIsSignedIn(true)} />
               </Route>
+              <Route path="/dashboard">
+                {!isSignedIn && <Redirect to="/" />}
+                <DashboardLazy />
+              </Route>
 
               <Route path="/" component={MarketingLazy} />
             </Switch>
@@ -44,6 +59,6 @@ export default () => {
           {/* <MarketingApp /> */}
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   )
 }
