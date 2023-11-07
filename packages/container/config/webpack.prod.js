566c0aeed88a4c351cb1b025587c73cca18111c5
Juan Carlos Valdés <idevkingos@gmail.com>
2023-11-07 12:14:30 -0300

    fixes

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -16,8 +16,8 @@ const prodConfig = {
       name: 'container',
       remotes: {
         marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-        auth: 'auth@http://localhost:8082/remoteEntry.js',
-        dashboard: 'dashboard@http://localhost:8083/remoteEntry.js',
+        auth: `auth@${domain}/remoteEntry.js`,
+        dashboard: `dashboard@${domain}/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
